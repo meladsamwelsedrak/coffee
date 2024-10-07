c9d47153d5ffbc9eb8651e233d433829e0ef902a
meladsamwelsedrak <ealsedrakmeladsum@gmail.com>
2024-10-07 22:55:21 +0300

    Add files via upload

---
+++ coffee.js
@@ -0,0 +1,24 @@
+let navbar = document.querySelector('.navbar');
+document.querySelector('#bard-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    cartItem .classList.remove('active');
+    searchForm .classList.remove('active');
+}
+let cartItem = document.querySelector('.cart-item-contact');
+document.querySelector('#shopping-btn').onclick = () =>{
+    cartItem .classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm .classList.remove('active');
+}
+let searchForm = document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm .classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem .classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    cartItem .classList.remove('active');
+    searchForm .classList.remove('active');
+}
